78e32bd235bf752fac1c7ba11179e1230ad0f5fa
Jared Johnson <jared@jaredjohnson.dev>
2020-10-25 13:47:50 -0400

    Add Todo skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Bingo } from "./bingo/Bingo";
 import { Cart } from "./cart/Cart";
+import { Todo } from "./todo/Todo";
 
 export const App = () => {
   return (
     <>
+      <h2>Todo</h2>
+      <Todo />
+
+      <hr />
+
       <h2>Cart</h2>
       <Cart />
 
